c52ba75ee193a4c117b72b8c8361d67a70eb883b
Cameron Fitzpatrick <45824254+FibreBit@users.noreply.github.com>
2020-11-04 17:00:22 +0000

    Create Cats_and_Dogs.java
    Create a Dog and a Cat class, each of which inherit from Animal. Each class should have a constructor which initializes their name and a hello method.
    The cat's hello method should return the string "Meow" and the dog's hello method should return the string  "Woof".

---
+++ Java/CA269 - Computer Programming 4(OO Programming)/Cats_and_Dogs.java
@@ -0,0 +1,22 @@
+public class Dog extends Animal
+{
+    public Dog(String s)
+    {
+        super(s);
+    }
+    public String hello()
+    {
+        return ("Woof");
+    }
+}
+public class Cat extends Animal
+{
+    public Cat(String s)
+    {
+        super(s);
+    }
+    public String hello()
+    {
+        return ("Meow");
+    }
+}
